9521df840766188649ccde4da29c542770e110e1
That411guy <mharrison747@gmail.com>
2024-08-27 01:32:53 -0400

    Added and removed sleep

--- games/pathoftitans/entrypoint.sh
+++ games/pathoftitans/entrypoint.sh
@@ -56,14 +56,13 @@ echo -e "${GREEN}---------------------------------------------------------------
 echo -e "${CYAN}Running on Ubuntu ${RED} $(cat /etc/debian_version)${NC}"
 echo -e "${GREEN}Current timezone: ${RED} $(date +"%Z %z")${NC}"
 echo -e "${GREEN}---------------------------------------------------------------------${NC}"
-sleep 5
 chmod +x /home/container/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping
 
 ## check for serverupdates
 if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
     cd /home/container
     echo -e "${BLUE}---------------------------------------------------------------------${NC}"
-    echo -e "${YELLOW}checkig for Server update. please wait...${NC}"
+    echo -e "${YELLOW}Checking for Server update. please wait...${NC}"
     echo -e "${BLUE}---------------------------------------------------------------------${NC}"
     export DOTNET_BUNDLE_EXTRACT_BASE_DIR=./temp/
     ./AlderonGamesCmd --game path-of-titans --server true --beta-branch $BETA_BRANCH --install-dir ./ --username $AG_SERVER_EMAIL --password $AG_SERVER_PASS
@@ -73,7 +72,7 @@ if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
     echo -e "${YELLOW}Not updating game server as auto update was set to 0. Starting Server${NC}"
     echo -e "${BLUE}---------------------------------------------------------------${NC}"
 fi
-
+sleep 5
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
