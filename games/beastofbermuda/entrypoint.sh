a98e31545d9d0332588e29aae2afae83741c158b
That411guy <mharrison747@gmail.com>
2024-08-31 22:41:54 -0400

    fixed colors and time

--- games/beastofbermuda/entrypoint.sh
+++ games/beastofbermuda/entrypoint.sh
@@ -5,13 +5,16 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
 NC='\033[0m'
 
 # Wait for the container to fully initialize
 sleep 1
 
-# Default the TZ environment variable to UTC.
-TZ=${TZ:-UTC}
+# Force the TZ environment variable to Eastern Standard Time
+TZ=America/New_York
 export TZ
 
 # Set environment variable that holds the Internal Docker IP
