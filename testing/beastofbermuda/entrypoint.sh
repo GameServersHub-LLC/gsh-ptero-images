b09819353e5fc2e57bfe8b8579aef247f35829b2
That411guy <mharrison747@gmail.com>
2024-09-08 20:08:52 -0400

    f

--- testing/beastofbermuda/entrypoint.sh
+++ testing/beastofbermuda/entrypoint.sh
@@ -113,5 +113,4 @@ MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-./BeastsOfBermuda/Binaries/Linux/BeastsOfBermudaServer -log
 eval ${MODIFIED_STARTUP}
