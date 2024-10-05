06104a4fc39b373f300b83b2cca31b22033c3aaf
That411guy <mharrison747@gmail.com>
2024-10-05 02:28:00 -0400

    now

--- testing/pathoftitans/entrypoint.sh
+++ testing/pathoftitans/entrypoint.sh
@@ -77,7 +77,13 @@ if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
     sleep 3
 fi
 
-##Setup RCON
+# Modify MaxPlayers in Game.ini using awk
+awk -v slots="{{SLOTS}}" '/\[\/Script\/PathOfTitans\.IGameSession\]/{print; flag=1; next} 
+flag && /MaxPlayers/{print "MaxPlayers=" slots; flag=0; next} 
+flag && /^$/{print "MaxPlayers=" slots; print; flag=0; next} 
+{print}' ./PathOfTitans/Saved/Config/LinuxServer/Game.ini > ./PathOfTitans/Saved/Config/LinuxServer/temp.ini && mv ./PathOfTitans/Saved/Config/LinuxServer/temp.ini ./PathOfTitans/Saved/Config/LinuxServer/Game.ini
+
+# Escape special characters in environment variables (if needed)
 ESCAPED_RCON_PORT=$(printf '%q' "$RCON_PORT")
 ESCAPED_RCON_PASSWORD=$(printf '%q' "$RCON_PASSWORD")
 
@@ -86,11 +92,19 @@ ESCAPED_RCON_PASSWORD=$(printf '%q' "$RCON_PASSWORD")
     rcon -s -a "localhost:$ESCAPED_RCON_PORT" -p "$ESCAPED_RCON_PASSWORD" "$cmd"
 done) < /dev/stdin &
 
-# MaxPlayers modification and server startup
-awk -v slots="{{SLOTS}}" '/\[\/Script\/PathOfTitans\.IGameSession\]/{print; flag=1; next} 
-flag && /MaxPlayers/{print "MaxPlayers=" slots; flag=0; next} 
-flag && /^$/{print "MaxPlayers=" slots; print; flag=0; next} 
-{print}' ./PathOfTitans/Saved/Config/LinuxServer/Game.ini > ./PathOfTitans/Saved/Config/LinuxServer/temp.ini && mv ./PathOfTitans/Saved/Config/LinuxServer/temp.ini ./PathOfTitans/Saved/Config/LinuxServer/Game.ini && ./PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping
+# Path of Titans server startup command with all parameters
+./PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping \
+    -ServerName="${SERVER_NAME}" \
+    -Port=$SERVER_PORT \
+    -BranchKey=$BETA_BRANCH \
+    $(if [ -n "$SERVER_PASSWORD" ]; then echo "-ServerPassword=\"${SERVER_PASSWORD}\""; fi) \
+    -AuthToken=$AG_AUTH_TOKEN \
+    -ServerGUID=$SERVER_GUID \
+    -Database=$DATABASE \
+    -nullRHI \
+    -rcon \
+    -log
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
