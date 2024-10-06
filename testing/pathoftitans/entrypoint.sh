242adeb80cd7364892af5997435d13efa47ee906
That411guy <mharrison747@gmail.com>
2024-10-05 21:10:05 -0400

    i

--- testing/pathoftitans/entrypoint.sh
+++ testing/pathoftitans/entrypoint.sh
@@ -74,7 +74,7 @@ if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
     echo -e "${BLUE}---------------------------------------------------------------${NC}"
     echo -e "${YELLOW}Not updating game server as auto update was set to 0. Starting Server${NC}"
     echo -e "${BLUE}---------------------------------------------------------------${NC}"
-    sleep 3
+    sleep 1
 fi
 
 # Escape special characters in environment variables (if needed)
