94f5d8b2cf3c595f5d478155f84a2f0c98ae2cda
That411guy <mharrison747@gmail.com>
2024-10-17 00:18:37 -0400

    t

--- alpha/draconia/entrypoint.sh
+++ alpha/draconia/entrypoint.sh
@@ -1,67 +1,36 @@
 #!/bin/bash
-#System variables
-clear
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-MAGENTA='\033[0;35m'
-CYAN='\033[0;36m'
-WHITE='\033[0;37m'
-NC='\033[0m'
 
-# Switch to the container's working directory
-cd /home/container
 
 # Wait for the container to fully initialize
 sleep 1
 
-# Force the TZ environment variable to Eastern Standard Time
-TZ=America/New_York
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
 export TZ
 
-# Make internal Docker IP address available to processes.
+# Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# system informations                                                           
-echo -e "${YELLOW} Made By                                                          ${NC}"
-echo -e "${MAGENTA}         GGGGGGGGGGGGG   SSSSSSSSSSSSSSS HHHHHHHHH     HHHHHHHHH ${NC}"
-echo -e "${MAGENTA}      GGG::::::::::::G SS:::::::::::::::SH:::::::H     H:::::::H ${NC}"
-echo -e "${MAGENTA}    GG:::::::::::::::GS:::::SSSSSS::::::SH:::::::H     H:::::::H ${NC}"
-echo -e "${MAGENTA}   G:::::GGGGGGGG::::GS:::::S     SSSSSSSHH::::::H     H::::::HH ${NC}"
-echo -e "${MAGENTA}  G:::::G       GGGGGGS:::::S              H:::::H     H:::::H   ${NC}"
-echo -e "${MAGENTA} G:::::G              S:::::S              H:::::H     H:::::H   ${NC}"
-echo -e "${MAGENTA} G:::::G               S::::SSSS           H::::::HHHHH::::::H   ${NC}"
-echo -e "${MAGENTA} G:::::G    GGGGGGGGGG  SS::::::SSSSS      H:::::::::::::::::H   ${NC}"
-echo -e "${MAGENTA} G:::::G    G::::::::G    SSS::::::::SS    H:::::::::::::::::H   ${NC}"
-echo -e "${MAGENTA} G:::::G    GGGGG::::G       SSSSSS::::S   H::::::HHHHH::::::H   ${NC}"
-echo -e "${MAGENTA} G:::::G        G::::G            S:::::S  H:::::H     H:::::H   ${NC}"
-echo -e "${MAGENTA}  G:::::G       G::::G            S:::::S  H:::::H     H:::::H   ${NC}"
-echo -e "${MAGENTA}   G:::::GGGGGGGG::::GSSSSSSS     S:::::SHH::::::H     H::::::HH ${NC}"
-echo -e "${MAGENTA}    GG:::::::::::::::GS::::::SSSSSS:::::SH:::::::H     H:::::::H ${NC}"
-echo -e "${MAGENTA}      GGG::::::GGG:::GS:::::::::::::::SS H:::::::H     H:::::::H ${NC}"
-echo -e "${MAGENTA}         GGGGGG   GGGG SSSSSSSSSSSSSSS   HHHHHHHHH     HHHHHHHHH ${NC}"
-echo -e "${YELLOW} If you need support please join our discord at: discord.gg/gsh   ${NC}"
+# Set environment for Steam Proton
+if [ -f "/usr/local/bin/proton" ]; then
+    if [ ! -z ${SRCDS_APPID} ]; then
+	    mkdir -p /home/container/.steam/steam/steamapps/compatdata/${SRCDS_APPID}
+        export STEAM_COMPAT_CLIENT_INSTALL_PATH="/home/container/.steam/steam"
+        export STEAM_COMPAT_DATA_PATH="/home/container/.steam/steam/steamapps/compatdata/${SRCDS_APPID}"
+        # Fix for pipx with protontricks
+        export PATH=$PATH:/root/.local/bin
+    else
+        echo -e "----------------------------------------------------------------------------------"
+        echo -e "WARNING!!! Proton needs variable SRCDS_APPID, else it will not work. Please add it"
+        echo -e "Server stops now"
+        echo -e "----------------------------------------------------------------------------------"
+        exit 0
+        fi
+fi
 
-
-echo -e "${WHITE} _____                                             _____ ${NC}"
-echo -e "${WHITE}( ___ )                                           ( ___ )${NC}"
-echo -e "${WHITE} |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | ${NC}"
-echo -e "${WHITE} |   |${MAGENTA} ·▄▄▄▄  ▄▄▄   ▄▄▄·  ▄▄·        ▐ ▄ ▪   ▄▄▄·  |   | ${NC}"
-echo -e "${WHITE} |   |${MAGENTA} ██▪ ██ ▀▄ █·▐█ ▀█ ▐█ ▌▪▪     •█▌▐███ ▐█ ▀█  |   | ${NC}"
-echo -e "${WHITE} |   |${MAGENTA} ▐█· ▐█▌▐▀▀▄ ▄█▀▀█ ██ ▄▄ ▄█▀▄ ▐█▐▐▌▐█·▄█▀▀█  |   | ${NC}"
-echo -e "${WHITE} |   |${MAGENTA} ██. ██ ▐█•█▌▐█ ▪▐▌▐███▌▐█▌.▐▌██▐█▌▐█▌▐█ ▪▐▌ |   | ${NC}"
-echo -e "${WHITE} |   |${MAGENTA} ▀▀▀▀▀• .▀  ▀ ▀  ▀ ·▀▀▀  ▀█▄▀▪▀▀ █▪▀▀▀ ▀  ▀  |   | ${NC}"
-echo -e "${WHITE} |   |${YELLOW} by that411guy ${WHITE}                          ${WHITE}|   | ${NC}"
-echo -e "${WHITE} |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| ${NC}"
-echo -e "${WHITE}(_____)                                           (_____)${NC}"
-echo -e "${GREEN} Current timezone:${WHITE} $TZ ${GREEN} Current Time: ${WHITE}$(date '+%A, %B %d, %Y %I:%M %p')"${NC}
-
-sleep 3
 # Switch to the container's working directory
 cd /home/container || exit 1
-
 
 ## just in case someone removed the defaults.
 if [ "${STEAM_USER}" == "" ]; then
