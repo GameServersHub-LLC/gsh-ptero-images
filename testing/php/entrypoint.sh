3ebc5cfb979124564eb4e36ed30c9cf2a16359b3
That411guy <mharrison747@gmail.com>
2025-01-20 02:34:08 -0500

    d

--- testing/php/entrypoint.sh
+++ testing/php/entrypoint.sh
@@ -4,9 +4,23 @@ cd /home/container
 # Output Current PHP Version
 php -v
 
+# Start Apache
+apache2ctl start
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Add command listener
+(
+    while read line; do
+        if [[ "$line" == "stop" ]]; then
+            pkill -SIGTERM apache2
+            break
+        fi
+        eval "$line"
+    done
+) &
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
