fb4f6e28742ff5bf4398074fd83ea01bcf1e9af3
That411guy <mharrison747@gmail.com>
2025-02-03 20:15:23 -0500

    w

--- testing/dayz/entrypoint.sh
+++ testing/dayz/entrypoint.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 ## File: DayZ Image - entrypoint.sh
@@ -29,24 +28,22 @@ NC='\033[0m' # No Color
 ## === DEFINE FUNCTIONS ===
 
 # Runs SteamCMD with specified variables and performs error handling.
-function RunSteamCMD { #[Input: int server=0 mod=1; int id]
-    # Clear previous SteamCMD log
-    if [[ -f "${STEAMCMD_LOG}" ]]; then
+RunSteamCMD() {
+    if [ -f "${STEAMCMD_LOG}" ]; then
         rm -f "${STEAMCMD_LOG:?}"
     fi
     
     updateAttempt=0
-    while (( $updateAttempt < $STEAMCMD_ATTEMPTS )); do # Loop for specified number of attempts
-        # Increment attempt counter
+    while [ $updateAttempt -lt $STEAMCMD_ATTEMPTS ]; do
         updateAttempt=$((updateAttempt+1))
         
-        if (( $updateAttempt > 1 )); then # Notify if not first attempt
+        if [ $updateAttempt -gt 1 ]; then
             echo -e "\t${YELLOW}Re-Attempting download/update in 3 seconds...${NC} (Attempt ${CYAN}${updateAttempt}${NC} of ${CYAN}${STEAMCMD_ATTEMPTS}${NC})\n"
             sleep 3
         fi
         
         # Check if updating server or mod
-        if [[ $1 == 0 ]]; then # Server
+        if [ $1 -eq 0 ]; then # Server
             ${STEAMCMD_DIR}/steamcmd.sh +force_install_dir /home/container "+login \"${STEAM_USER}\" \"${STEAM_PASS}\"" +app_update $2 $extraFlags $validateServer +quit | tee -a "${STEAMCMD_LOG}"
         else # Mod
             ${STEAMCMD_DIR}/steamcmd.sh "+login \"${STEAM_USER}\" \"${STEAM_PASS}\"" +workshop_download_item $GAME_ID $2 +quit | tee -a "${STEAMCMD_LOG}"
@@ -56,37 +53,37 @@ function RunSteamCMD { #[Input: int server=0 mod=1; int id]
         steamcmdExitCode=${PIPESTATUS[0]}
         # Catch errors (ignore setlocale, SDL, steamservice, thread priority, and libcurl warnings)
         loggedErrors=$(grep -i "error\|failed" "${STEAMCMD_LOG}" | grep -iv "setlocal\|SDL\|steamservice\|thread\|libcurl")
-        if [[ -n ${loggedErrors} ]]; then
+        if [ -n "${loggedErrors}" ]; then
             # Soft errors
-            if [[ -n $(grep -i "Timeout downloading item" "${STEAMCMD_LOG}") ]]; then # Mod download timeout
+            if [ -n "$(grep -i "Timeout downloading item" "${STEAMCMD_LOG}")" ]; then # Mod download timeout
                 echo -e "\n${YELLOW}[UPDATE]: ${NC}Timeout downloading Steam Workshop mod: \"${CYAN}${modName}${NC}\" (${CYAN}${2}${NC})"
                 echo -e "\t(This is expected for particularly large mods)"
-            elif [[ -n $(grep -i "0x402\|0x6\|0x602" "${STEAMCMD_LOG}") ]]; then # Connection issue with Steam
+            elif [ -n "$(grep -i "0x402\|0x6\|0x602" "${STEAMCMD_LOG}")" ]; then # Connection issue with Steam
                 echo -e "\n${YELLOW}[UPDATE]: ${NC}Connection issue with Steam servers."
                 echo -e "\t(Steam servers may currently be down, or a connection cannot be made reliably)"
             # Hard errors
-            elif [[ -n $(grep -i "Password check for AppId" "${STEAMCMD_LOG}") ]]; then # Incorrect beta branch password
+            elif [ -n "$(grep -i "Password check for AppId" "${STEAMCMD_LOG}")" ]; then # Incorrect beta branch password
                 echo -e "\n${RED}[UPDATE]: ${YELLOW}Incorrect password given for beta branch. ${CYAN}Skipping download...${NC}"
                 echo -e "\t(Check your \"[ADVANCED] EXTRA FLAGS FOR STEAMCMD\" startup parameter)"
                 break
             # Fatal errors
-            elif [[ -n $(grep -i "Invalid Password\|two-factor\|No subscription" "${STEAMCMD_LOG}") ]]; then # Wrong username/password, Steam Guard is turned on, or host is using anonymous account
+            elif [ -n "$(grep -i "Invalid Password\|two-factor\|No subscription" "${STEAMCMD_LOG}")" ]; then # Wrong username/password, Steam Guard is turned on, or host is using anonymous account
                 echo -e "\n${RED}[UPDATE]: Cannot login to Steam - Improperly configured account and/or credentials"
                 echo -e "\t${YELLOW}Please contact your administrator/host and give them the following message:${NC}"
                 echo -e "\t${CYAN}Your Egg, or your client's server, is not configured with valid Steam credentials.${NC}"
                 echo -e "\t${CYAN}Either the username/password is wrong, or Steam Guard is not properly configured\n\taccording to this egg's documentation/README.${NC}\n"
                 exit 1
-            elif [[ -n $(grep -i "Download item" "${STEAMCMD_LOG}") ]]; then # Steam account does not own base game for mod downloads, or unknown
+            elif [ -n "$(grep -i "Download item" "${STEAMCMD_LOG}")" ]; then # Steam account does not own base game for mod downloads, or unknown
                 echo -e "\n${RED}[UPDATE]: Cannot download mod - Download failed"
                 echo -e "\t${YELLOW}While unknown, this error is likely due to your host's Steam account not owning the base game.${NC}"
                 echo -e "\t${YELLOW}(Please contact your administrator/host if this issue persists)${NC}\n"
                 exit 1
-            elif [[ -n $(grep -i "0x202\|0x212" "${STEAMCMD_LOG}") ]]; then # Not enough disk space
+            elif [ -n "$(grep -i "0x202\|0x212" "${STEAMCMD_LOG}")" ]; then # Not enough disk space
                 echo -e "\n${RED}[UPDATE]: Unable to complete download - Not enough storage"
                 echo -e "\t${YELLOW}You have run out of your allotted disk space.${NC}"
                 echo -e "\t${YELLOW}Please contact your administrator/host for potential storage upgrades.${NC}\n"
                 exit 1
-            elif [[ -n $(grep -i "0x606" "${STEAMCMD_LOG}") ]]; then # Disk write failure
+            elif [ -n "$(grep -i "0x606" "${STEAMCMD_LOG}")" ]; then # Disk write failure
                 echo -e "\n${RED}[UPDATE]: Unable to complete download - Disk write failure"
                 echo -e "\t${YELLOW}This is normally caused by directory permissions issues,\n\tbut could be a more serious hardware issue.${NC}"
                 echo -e "\t${YELLOW}(Please contact your administrator/host if this issue persists)${NC}\n"
@@ -96,13 +93,13 @@ function RunSteamCMD { #[Input: int server=0 mod=1; int id]
                 echo -e "\t(Please contact your administrator/host if this issue persists)"
                 break
             fi
-        elif [[ $steamcmdExitCode != 0 ]]; then # Unknown fatal error
+        elif [ $steamcmdExitCode -ne 0 ]; then # Unknown fatal error
             echo -e "\n${RED}[UPDATE]: SteamCMD has crashed for an unknown reason!${NC} (Exit code: ${CYAN}${steamcmdExitCode}${NC})"
             echo -e "\t${YELLOW}(Please contact your administrator/host for support)${NC}\n"
             cp -r /tmp/dumps /home/container/dumps
             exit $steamcmdExitCode
         else # Success!
-            if [[ $1 == 0 ]]; then # Server
+            if [ $1 -eq 0 ]; then # Server
                 echo -e "\n${GREEN}[UPDATE]: Game server is up to date!${NC}"
             else # Mod
                 # Move the downloaded mod to the root directory, and replace existing mod if needed
@@ -119,8 +116,8 @@ function RunSteamCMD { #[Input: int server=0 mod=1; int id]
             fi
             break
         fi
-        if (( $updateAttempt == $STEAMCMD_ATTEMPTS )); then # Notify if failed last attempt
-            if [[ $1 == 0 ]]; then # Server
+        if [ $updateAttempt -eq $STEAMCMD_ATTEMPTS ]; then # Notify if failed last attempt
+            if [ $1 -eq 0 ]; then # Server
                 echo -e "\t${RED}Final attempt made! ${YELLOW}Unable to complete game server update. ${CYAN}Skipping...${NC}"
                 echo -e "\t(Please try again at a later time)"
                 sleep 3
@@ -134,21 +131,19 @@ function RunSteamCMD { #[Input: int server=0 mod=1; int id]
 }
 
 # Takes a directory (string) as input, and recursively makes all files & folders lowercase.
-function ModsLowercase {
+ModsLowercase() {
     echo -e "\n\tMaking mod ${CYAN}$1${NC} files/folders lowercase..."
-    for SRC in `find ./$1 -depth`
-    do
+    find ./$1 -depth | while read SRC; do
         DST=`dirname "${SRC}"`/`basename "${SRC}" | tr '[A-Z]' '[a-z]'`
-        if [ "${SRC}" != "${DST}" ]
-        then
+        if [ "${SRC}" != "${DST}" ]; then
             [ ! -e "${DST}" ] && mv -T "${SRC}" "${DST}"
         fi
     done
 }
 
 # Removes duplicate items from a semicolon delimited string
-function RemoveDuplicates { #[Input: str - Output: printf of new str]
-    if [[ -n $1 ]]; then # If nothing to compare, skip to prevent extra semicolon being returned
+RemoveDuplicates() {
+    if [ -n "$1" ]; then
         echo $1 | sed -e 's/;/\n/g' | sort -u | xargs printf '%s;'
     fi
 }
@@ -166,24 +161,24 @@ export INTERNAL_IP
 cd /home/container || exit 1
 
 # Collect and parse all specified mods
-if [[ -n ${MODIFICATIONS} ]] && [[ ${MODIFICATIONS} != *\; ]]; then # Add manually specified mods to the client-side mods list, while checking for trailing semicolon
+if [ -n "${MODIFICATIONS}" ] && [ "${MODIFICATIONS}" != *\; ]; then # Add manually specified mods to the client-side mods list, while checking for trailing semicolon
     CLIENT_MODS="${MODIFICATIONS};"
 else
     CLIENT_MODS=${MODIFICATIONS}
 fi
 # If the mod list file exists and is valid, parse and add mods to the client-side mods list
-if [[ -f ${MOD_FILE} ]] && [[ -n "$(cat ${MOD_FILE} | grep 'Created by DayZ Launcher')" ]]; then
+if [ -f "${MOD_FILE}" ] && [ -n "$(cat ${MOD_FILE} | grep 'Created by DayZ Launcher')" ]; then
     CLIENT_MODS+=$(cat ${MOD_FILE} | grep 'id=' | cut -d'=' -f3 | cut -d'"' -f1 | xargs printf '@%s;')
-elif [[ -n "${MOD_FILE}" ]]; then # If MOD_FILE is not null, warn user file is missing or invalid
+elif [ -n "${MOD_FILE}" ]; then # If MOD_FILE is not null, warn user file is missing or invalid
     echo -e "\n${YELLOW}[STARTUP_WARN]: DayZ Modlist file \"${CYAN}${MOD_FILE}${YELLOW}\" could not be found, or is invalid!${NC}"
     echo -e "\tEnsure your uploaded modlist's file name matches your Startup Parameter."
     echo -e "\tOnly files exported from a DayZ Launcher are permitted."
-    if [[ -n "${CLIENT_MODS}" ]]; then
+    if [ -n "${CLIENT_MODS}" ]; then
         echo -e "\t${CYAN}Reverting to the manual mod list...${NC}"
     fi
 fi
 # Add server mods to the master mods list, while checking for trailing semicolon
-if [[ -n ${SERVERMODS} ]] && [[ ${SERVERMODS} != *\; ]]; then
+if [ -n "${SERVERMODS}" ] && [ "${SERVERMODS}" != *\; ]; then
     allMods="${SERVERMODS};"
 else
     allMods=${SERVERMODS}
@@ -194,14 +189,14 @@ allMods=$(RemoveDuplicates ${allMods}) # Remove duplicate mods from allMods, if 
 allMods=$(echo $allMods | sed -e 's/;/ /g') # Convert from string to array
 
 # Update everything (server and mods), if specified
-if [[ ${UPDATE_SERVER} == 1 ]]; then
+if [ "${UPDATE_SERVER}" -eq 1 ]; then
     echo -e "\n${GREEN}[STARTUP]: ${CYAN}Starting checks for all updates...${NC}"
     echo -e "(It is okay to ignore any \"SDL\" and \"thread priority\" errors during this process)\n"
     
     ## Update game server
     echo -e "${GREEN}[UPDATE]:${NC} Checking for game server updates with App ID: ${CYAN}${STEAMCMD_APPID}${NC}..."
     
-    if [[ ${VALIDATE_SERVER} == 1 ]]; then # Validate will be added as a parameter if specified
+    if [ "${VALIDATE_SERVER}" -eq 1 ]; then # Validate will be added as a parameter if specified
         echo -e "\t${CYAN}File validation enabled.${NC} (This may take extra time to complete)"
         validateServer="validate"
     else
@@ -209,7 +204,7 @@ if [[ ${UPDATE_SERVER} == 1 ]]; then
     fi
     
     # Determine what extra flags should be set
-    if [[ -n ${STEAMCMD_EXTRA_FLAGS} ]]; then
+    if [ -n "${STEAMCMD_EXTRA_FLAGS}" ]; then
         echo -e "\t(${YELLOW}Advanced${NC}) Extra SteamCMD flags specified: ${CYAN}${STEAMCMD_EXTRA_FLAGS}${NC}\n"
         extraFlags=${STEAMCMD_EXTRA_FLAGS}
     else
@@ -220,26 +215,26 @@ if [[ ${UPDATE_SERVER} == 1 ]]; then
     RunSteamCMD 0 ${STEAMCMD_APPID}
     
     ## Update mods
-    if [[ -n $allMods ]] && [[ ${DISABLE_MOD_UPDATES} != 1 ]]; then
+    if [ -n "$allMods" ] && [ "${DISABLE_MOD_UPDATES}" -ne 1 ]; then
         echo -e "\n${GREEN}[UPDATE]:${NC} Checking all ${CYAN}Steam Workshop mods${NC} for updates..."
         for modID in $(echo $allMods | sed -e 's/@//g')
         do
-            if [[ $modID =~ ^[0-9]+$ ]]; then # Only check mods that are in ID-form
+            if [ $modID -eq $modID ] 2>/dev/null; then # Only check mods that are in ID-form
                 # Get mod's latest update in epoch time from its Steam Workshop changelog page
                 latestUpdate=$(curl -sL https://steamcommunity.com/sharedfiles/filedetails/changelog/$modID | grep '<p id=' | head -1 | cut -d'"' -f2)
                 # If the update time is valid and newer than the local directory's creation date, or the mod hasn't been downloaded yet, download the mod
-                if [[ ! -d @$modID ]] || [[ ( -n $latestUpdate ) && ( $latestUpdate =~ ^[0-9]+$ ) && ( $latestUpdate > $(find @$modID | head -1 | xargs stat -c%Y) ) ]]; then
+                if [ ! -d @$modID ] || [ -n "$latestUpdate" ] && [ "$latestUpdate" -eq "$latestUpdate" ] 2>/dev/null && [ "$latestUpdate" -gt $(find @$modID | head -1 | xargs stat -c%Y) ]; then
                     # Get the mod's name from the Workshop page as well
                     modName=$(curl -sL https://steamcommunity.com/sharedfiles/filedetails/changelog/$modID | grep 'workshopItemTitle' | cut -d'>' -f2 | cut -d'<' -f1)
-                    if [[ -z $modName ]]; then # Set default name if unavailable
+                    if [ -z "$modName" ]; then # Set default name if unavailable
                         modName="[NAME UNAVAILABLE]"
                     fi
-                    if [[ ! -d @$modID ]]; then
+                    if [ ! -d @$modID ]; then
                         echo -e "\n${GREEN}[UPDATE]:${NC} Downloading new Mod: \"${CYAN}${modName}${NC}\" (${CYAN}${modID}${NC})"
                     else
                         echo -e "\n${GREEN}[UPDATE]:${NC} Mod update found for: \"${CYAN}${modName}${NC}\" (${CYAN}${modID}${NC})"
                     fi
-                    if [[ -n $latestUpdate ]] && [[ $latestUpdate =~ ^[0-9]+$ ]]; then # Notify last update date, if valid
+                    if [ -n "$latestUpdate" ] && [ "$latestUpdate" -eq "$latestUpdate" ] 2>/dev/null; then # Notify last update date, if valid
                         echo -e "\tMod was last updated: ${CYAN}$(date -d @${latestUpdate})${NC}"
                     fi
                     
@@ -257,7 +252,7 @@ if [[ ${UPDATE_SERVER} == 1 ]]; then
 fi
 
 # Check if specified server binary exists.
-if [[ ! -f ./${SERVER_BINARY} ]]; then
+if [ ! -f ./${SERVER_BINARY} ]; then
     echo -e "\n${RED}[STARTUP_ERR]: Specified DayZ server binary could not be found in the root directory!${NC}"
     echo -e "${YELLOW}Please do the following to resolve this issue:${NC}"
     echo -e "\t${CYAN}- Double check your \"Server Binary\" Startup Variable is correct.${NC}"
@@ -267,7 +262,7 @@ if [[ ! -f ./${SERVER_BINARY} ]]; then
 fi
 
 # Make mods lowercase, if specified
-if [[ ${MODS_LOWERCASE} == "1" ]]; then
+if [ "${MODS_LOWERCASE}" -eq 1 ]; then
     for modDir in $allMods
     do
         ModsLowercase $modDir
