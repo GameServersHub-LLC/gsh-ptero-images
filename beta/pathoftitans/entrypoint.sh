df9c61a338964cab703e5062a327d28685791f6d
That411Guy <mharrison747@gmail.com>
2026-01-29 22:02:48 -0500

    Update Dockerfile and entrypoint script for Path of Titans: enhance package installation, update timezone handling, and improve startup messages

--- beta/pathoftitans/entrypoint.sh
+++ beta/pathoftitans/entrypoint.sh
@@ -9,6 +9,11 @@ MAGENTA='\033[0;35m'
 CYAN='\033[0;36m'
 WHITE='\033[0;37m'
 NC='\033[0m'
+BOLD='\033[1m'
+BLINK='\033[5m'
+BRIGHT_GREEN='\033[1;32m'
+BRIGHT_YELLOW='\033[1;33m'
+BRIGHT_CYAN='\033[1;36m'
 
 # Switch to the container's working directory
 cd /home/container
@@ -16,8 +21,8 @@ cd /home/container
 # Wait for the container to fully initialize
 sleep 1
 
-# Force the TZ environment variable to Eastern Standard Time
-TZ=America/New_York
+# Force the TZ environment variable to UTC
+TZ=UTC
 export TZ
 
 # Make internal Docker IP address available to processes.
@@ -25,73 +30,61 @@ INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
 # system informations                                                           
-echo -e "${YELLOW} Made By                                                          ${NC}"
-echo -e "${MAGENTA}         GGGGGGGGGGGGG   SSSSSSSSSSSSSSS HHHHHHHHH     HHHHHHHHH ${NC}"
-echo -e "${MAGENTA}      GGG::::::::::::G SS:::::::::::::::SH:::::::H     H:::::::H ${NC}"
-echo -e "${MAGENTA}    GG:::::::::::::::GS:::::SSSSSS::::::SH:::::::H     H:::::::H ${NC}"
-echo -e "${MAGENTA}   G:::::GGGGGGGG::::GS:::::S     SSSSSSSHH::::::H     H::::::HH ${NC}"
-echo -e "${MAGENTA}  G:::::G       GGGGGGS:::::S              H:::::H     H:::::H   ${NC}"
-echo -e "${MAGENTA} G:::::G              S:::::S              H:::::H     H:::::H   ${NC}"
-echo -e "${MAGENTA} G:::::G               S::::SSSS           H::::::HHHHH::::::H   ${NC}"
-echo -e "${MAGENTA} G:::::G    GGGGGGGGGG  SS::::::SSSSS      H:::::::::::::::::H   ${NC}"
-echo -e "${MAGENTA} G:::::G    G::::::::G    SSS::::::::SS    H:::::::::::::::::H   ${NC}"
-echo -e "${MAGENTA} G:::::G    GGGGG::::G       SSSSSS::::S   H::::::HHHHH::::::H   ${NC}"
-echo -e "${MAGENTA} G:::::G        G::::G            S:::::S  H:::::H     H:::::H   ${NC}"
-echo -e "${MAGENTA}  G:::::G       G::::G            S:::::S  H:::::H     H:::::H   ${NC}"
-echo -e "${MAGENTA}   G:::::GGGGGGGG::::GSSSSSSS     S:::::SHH::::::H     H::::::HH ${NC}"
-echo -e "${MAGENTA}    GG:::::::::::::::GS::::::SSSSSS:::::SH:::::::H     H:::::::H ${NC}"
-echo -e "${MAGENTA}      GGG::::::GGG:::GS:::::::::::::::SS H:::::::H     H:::::::H ${NC}"
-echo -e "${MAGENTA}         GGGGGG   GGGG SSSSSSSSSSSSSSS   HHHHHHHHH     HHHHHHHHH ${NC}"
-echo -e "${YELLOW} If you need support please join our discord at: discord.gg/gsh   ${NC}"
-echo -e "${WHITE} _____                                                                                           _____ ${NC}"
-echo -e "${WHITE}( ___ )                                                                                         ( ___ )${NC}"
-echo -e "${WHITE} |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | ${NC}"
-echo -e "${WHITE} |   |${RED} ▄▄▄· ▄▄▄· ▄▄▄▄▄ ▄ .▄          ·▄▄▄    ▄▄▄▄▄▪  ▄▄▄▄▄ ▄▄▄·  ▐ ▄ .▄▄ ·     ▄▄▄ . ▄▄ •  ▄▄ •  ${WHITE}|   | ${NC}"
-echo -e "${WHITE} |   |${RED}▐█ ▄█▐█ ▀█ •██  ██▪▐█    ▪     ▐▄▄·    •██  ██ •██  ▐█ ▀█ •█▌▐█▐█ ▀.     ▀▄.▀·▐█ ▀ ▪▐█ ▀ ▪ ${WHITE}|   | ${NC}"
-echo -e "${WHITE} |   |${RED} ██▀·▄█▀▀█  ▐█.▪██▀▐█     ▄█▀▄ ██▪      ▐█.▪▐█· ▐█.▪▄█▀▀█ ▐█▐▐▌▄▀▀▀█▄    ▐▀▀▪▄▄█ ▀█▄▄█ ▀█▄ ${WHITE}|   | ${NC}"
-echo -e "${WHITE} |   |${RED}▐█▪·•▐█ ▪▐▌ ▐█▌·██▌▐▀    ▐█▌.▐▌██▌.     ▐█▌·▐█▌ ▐█▌·▐█ ▪▐▌██▐█▌▐█▄▪▐█    ▐█▄▄▌▐█▄▪▐█▐█▄▪▐█ ${WHITE}|   | ${NC}"
-echo -e "${WHITE} |   |${RED}.▀    ▀  ▀  ▀▀▀ ▀▀▀ ·     ▀█▄▀▪▀▀▀      ▀▀▀ ▀▀▀ ▀▀▀  ▀  ▀ ▀▀ █▪ ▀▀▀▀      ▀▀▀ ·▀▀▀▀ ·▀▀▀▀  ${WHITE}|   | ${NC}"
-echo -e "${WHITE} |   |${YELLOW} by that411guy ${WHITE}                                                                            ${WHITE}|   | ${NC}"
-echo -e "${WHITE} |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| ${NC}"
-echo -e "${WHITE}(_____)                                                                                         (_____)${NC}"
-echo -e "${GREEN} Current timezone:${WHITE} $TZ ${GREEN} Current Time: ${WHITE}$(date '+%A, %B %d, %Y %I:%M %p')"${NC}
+echo -e "${WHITE}                  ${YELLOW}Made By         ${MAGENTA}GGGGGGGGGGGGG   SSSSSSSSSSSSSSS HHHHHHHHH     HHHHHHHHH ${NC}"
+echo -e "${MAGENTA}                               GGG${WHITE}::::::::::::${MAGENTA}G SS${WHITE}:::::::::::::::${MAGENTA}SH${WHITE}:::::::${MAGENTA}H     H${WHITE}:::::::${MAGENTA}H ${NC}"
+echo -e "${MAGENTA}                             GG${WHITE}:::::::::::::::${MAGENTA}GS${WHITE}:::::${MAGENTA}SSSSSS${WHITE}::::::${MAGENTA}SH${WHITE}:::::::${MAGENTA}H     H${WHITE}:::::::${MAGENTA}H ${NC}"
+echo -e "${MAGENTA}                            G${WHITE}:::::${MAGENTA}GGGGGGGG${WHITE}::::${MAGENTA}GS${WHITE}:::::${MAGENTA}S     SSSSSSSHH${WHITE}::::::${MAGENTA}H     H${WHITE}::::::${MAGENTA}HH ${NC}"
+echo -e "${MAGENTA}                           G${WHITE}:::::${MAGENTA}G       GGGGGGS${WHITE}:::::${MAGENTA}S              H${WHITE}:::::${MAGENTA}H     H${WHITE}:::::${MAGENTA}H   ${NC}"
+echo -e "${MAGENTA}                          G${WHITE}:::::${MAGENTA}G              S${WHITE}:::::${MAGENTA}S              H${WHITE}:::::${MAGENTA}H     H${WHITE}:::::${MAGENTA}H   ${NC}"
+echo -e "${MAGENTA}                          G${WHITE}:::::${MAGENTA}G               S${WHITE}::::${MAGENTA}SSSS           H${WHITE}::::::${MAGENTA}HHHHH${WHITE}::::::${MAGENTA}H   ${NC}"
+echo -e "${MAGENTA}                          G${WHITE}:::::${MAGENTA}G    GGGGGGGGGG  SS${WHITE}::::::${MAGENTA}SSSSS      H${WHITE}:::::::::::::::::${MAGENTA}H   ${NC}"
+echo -e "${MAGENTA}                          G${WHITE}:::::${MAGENTA}G    G${WHITE}::::::::${MAGENTA}G    SSS${WHITE}::::::::${MAGENTA}SS    H${WHITE}:::::::::::::::::${MAGENTA}H   ${NC}"
+echo -e "${MAGENTA}                          G${WHITE}:::::${MAGENTA}G    GGGGG${WHITE}::::${MAGENTA}G       SSSSSS${WHITE}::::${MAGENTA}S   H${WHITE}::::::${MAGENTA}HHHHH${WHITE}::::::${MAGENTA}H   ${NC}"
+echo -e "${MAGENTA}                          G${WHITE}:::::${MAGENTA}G        G${WHITE}::::${MAGENTA}G            S${WHITE}:::::${MAGENTA}S  H${WHITE}:::::${MAGENTA}H     H${WHITE}:::::${MAGENTA}H   ${NC}"
+echo -e "${MAGENTA}                           G${WHITE}:::::${MAGENTA}G       G${WHITE}::::${MAGENTA}G            S${WHITE}:::::${MAGENTA}S  H${WHITE}:::::${MAGENTA}H     H${WHITE}:::::${MAGENTA}H   ${NC}"
+echo -e "${MAGENTA}                            G${WHITE}:::::${MAGENTA}GGGGGGGG${WHITE}::::${MAGENTA}GSSSSSSS     S${WHITE}:::::${MAGENTA}SHH${WHITE}::::::${MAGENTA}H     H${WHITE}::::::${MAGENTA}HH ${NC}"
+echo -e "${MAGENTA}                             GG${WHITE}:::::::::::::::${MAGENTA}GS${WHITE}::::::${MAGENTA}SSSSSS${WHITE}:::::${MAGENTA}SH${WHITE}:::::::${MAGENTA}H     H${WHITE}:::::::${MAGENTA}H ${NC}"
+echo -e "${MAGENTA}                               GGG${WHITE}::::::${MAGENTA}GGG${WHITE}:::${MAGENTA}GS${WHITE}:::::::::::::::${MAGENTA}SS H${WHITE}:::::::${MAGENTA}H     H${WHITE}:::::::${MAGENTA}H ${NC}"
+echo -e "${MAGENTA}                                  GGGGGG   GGGG SSSSSSSSSSSSSSS   HHHHHHHHH     HHHHHHHHH ${NC}"
+echo -e "${WHITE} _______                                                                                                            _______      ${NC}"
+echo -e "${WHITE}( _____ )  ${YELLOW}For need support, join our discord at: ${BLUE}discord.gg/gsh                                                   ${WHITE}( _____ )     ${NC}"
+echo -e "${WHITE} |     |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|     |      ${NC}"
+echo -e "${WHITE} |     |${YELLOW}   ███${YELLOW}███   ▄▄▄     ▄▄▄█████${RED}▓ ${YELLOW}██${RED}░ ${YELLOW}██${RED}     ▒${YELLOW}█████    █████${RED}▒ ${YELLOW}▄▄▄█████${RED}▓${YELLOW} ▄▄  █████${RED}▓${YELLOW}▄▄▄      ███▄    █  ██████  ${WHITE}  |     |      ${NC}"
+echo -e "${WHITE} |     |${RED}   ▓${YELLOW}██${RED}░  ${YELLOW}██${RED}▒▒${YELLOW}████▄${RED}   ▓  ${YELLOW}██${RED}▒ ▓▒▓${YELLOW}██${RED}░ ${YELLOW}██${RED}▒   ▒${YELLOW}██${RED}▒  ${YELLOW}██${RED}▒▓${YELLOW}██${RED}   ▒  ▓  ${YELLOW}██${RED}▒ ▓▓${YELLOW}██${RED}▓  ${YELLOW}██${RED}▒ ▓▒${YELLOW}████▄${RED}    ${YELLOW}██ ▀█   █${RED}▒${YELLOW}██${RED}    ▒ ${WHITE}  |     |      ${NC}"
+echo -e "${WHITE} |     |${RED}   ▓${YELLOW}██${RED}░ ${YELLOW}██${RED}▓▒▒${YELLOW}██  ▀█▄${RED} ▒ ▓${YELLOW}██${RED}░ ▒░▒${YELLOW}██▀▀██${RED}░   ▒${YELLOW}██${RED}░  ${YELLOW}██${RED}▒▒${YELLOW}████${RED} ░  ▒ ▓${YELLOW}██${RED}░ ▒▒${YELLOW}██${RED}▒ ▓${YELLOW}██${RED}░ ▒▒${YELLOW}██  ▀█▄${RED} ▓${YELLOW}██  ▀█ ██${RED}░ ▓${YELLOW}██▄   ${WHITE}  |     |      ${NC}"
+echo -e "${WHITE} |     |   ${RED}▒${YELLOW}██▄█${RED}▓▒ ▒░${YELLOW}██▄▄▄▄██${RED}░ ▓${YELLOW}██${RED}▓ ░ ░▓${YELLOW}█${RED} ░${YELLOW}██${RED}    ▒${YELLOW}██${RED}   ${YELLOW}██${RED}░░▓${YELLOW}█${RED}▒  ░  ░ ▓${YELLOW}██${RED}▓ ░░${YELLOW}██${RED}░ ▓${YELLOW}██${RED}▓ ░░${YELLOW}██▄▄▄▄██${RED}▓${YELLOW}██${RED}▒  ${YELLOW}▐▌██${RED}▒ ▒   ${YELLOW}██${RED}▒${WHITE}  |     |      ${NC}"
+echo -e "${WHITE} |     |   ${RED}▒${YELLOW}██${RED}▒ ░  ░ ▓${YELLOW}█${RED}   ▓${YELLOW}██${RED}▒ ▒${YELLOW}██${RED}▒ ░ ░▓${YELLOW}█${RED}▒░${YELLOW}██${RED}▓   ░ ${YELLOW}████${RED}▓▒░░▒${YELLOW}█${RED}░       ▒${YELLOW}██${RED}▒ ░░${YELLOW}██${RED}░ ▒${YELLOW}██${RED}▒ ░ ▓${YELLOW}█${RED}   ▓${YELLOW}██${RED}▒${YELLOW}██${RED}░   ▓${YELLOW}██${RED}▒${YELLOW}██████${RED}▒▒${WHITE}  |     |      ${NC}"
+echo -e "${WHITE} |     |   ${RED}▒${YELLOW}█${RED}▒░ ░  ░ ▒▒   ▓▒${YELLOW}█${RED}░ ▒ ░░    ▒ ░░▒░▒   ░ ▒░▒░▒░  ▒ ░       ▒ ░░  ░▓   ▒ ░░   ▒▒   ▓▒${YELLOW}█${RED}░ ▒░   ▒ ▒▒ ▒▓▒ ▒ ░${WHITE}  |     |      ${NC}"
+echo -e "${WHITE} |     |${RED}   ░▒ ░       ▒   ▒▒ ░   ░     ▒ ░▒░ ░     ░ ▒ ▒░  ░           ░    ▒ ░   ░     ▒   ▒▒ ░ ░░   ░ ▒░ ░▒  ░ ░${WHITE}  |     |      ${NC}"
+echo -e "${WHITE} |_____|${WHITE}~~~${RED}░░${WHITE}~~~~~~~~~${RED}░${WHITE}~~~${RED}▒${WHITE}~~~~${RED}░${WHITE}~~~~~~~${RED}░${WHITE}~~${RED}░░${WHITE}~${RED}░${WHITE}~~~${RED}░${WHITE}~${RED}░${WHITE}~${RED}░${WHITE}~${RED}▒${WHITE}~~~~~${RED}░${WHITE}~~~~~~~${RED}░${WHITE}~~~~~~${RED}▒${WHITE}~${RED}░${WHITE}~${RED}░${WHITE}~~~~~~~${RED}░${WHITE}~~~${RED}▒${WHITE}~~~~~${RED}░${WHITE}~~~${RED}░${WHITE}~${RED}░░${WHITE}~~${RED}░${WHITE}~~${RED}░${WHITE}~~~~${WHITE}|_____|      ${NC}"
+echo -e "${WHITE}(_______)${YELLOW}                                   🎮  SERVER STARTING UP... 🎮                         ${WHITE}                    (_______)     ${NC}"
+echo -e "${GREEN} Timezone:${WHITE} $TZ ${GREEN} Time: ${WHITE}$(date '+%A, %B %d, %Y %I:%M %p')"${NC} 
 UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 UE_PROJECT_ROOT=$(dirname "$UE_TRUE_SCRIPT_NAME")
 chmod +x /home/container/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping
 sleep 3
 
-## Check for server updates
-if [ -z "${AUTO_UPDATE}" ] || [ "${AUTO_UPDATE}" == "1" ]; then
+## check for serverupdates
+if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
     cd /home/container
     echo -e "${BLUE}---------------------------------------------------------------------${NC}"
-    echo -e "${YELLOW}Checking for Server Updates. please wait...${NC}"
+    echo -e "${YELLOW}    Checking for Server Updates. please wait...${NC}"
     echo -e "${BLUE}---------------------------------------------------------------------${NC}"
-    sleep 2
+    sleep 1
     export DOTNET_BUNDLE_EXTRACT_BASE_DIR=./temp/
-    
-    # Check if EXTRA_ARGS contains a numeric value and construct the hotfix command
-    if [[ "${HOT_FIX}" =~ ^[0-9]+$ ]]; then
-        HOTFIX_ARG="--hotfix ${HOT_FIX}"
-    else
-        HOTFIX_ARG=""
-    fi
-
-    ./AlderonGamesCmd --game path-of-titans --server true --beta-branch $BETA_BRANCH --install-dir ./ --username $AG_SERVER_EMAIL --password $AG_SERVER_PASS $HOTFIX_ARG
+    ./AlderonGamesCmd --game path-of-titans --server true --beta-branch $BETA_BRANCH --install-dir ./ --username $AG_SERVER_EMAIL --password $AG_SERVER_PASS --hotfix 23
     chmod +x /home/container/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping
-else
-    echo -e "${BLUE}---------------------------------------------------------------${NC}"
-    echo -e "${YELLOW}Not updating game server as auto update was set to false. Starting Server${NC}"
-    echo -e "${BLUE}---------------------------------------------------------------${NC}"
-    sleep 2
+ else
+    echo -e "${BLUE}-------------------------------------------------------------------------------------${NC}"
+    echo -e "${YELLOW}    Not updating game server as auto update was set to false. Starting Server${NC}"
+    echo -e "${BLUE}-------------------------------------------------------------------------------------${NC}"
+    sleep 3
 fi
 
-# Escape special characters in environment variables (if needed)
-ESCAPED_RCON_PORT=$(printf '%q' "$RCON_PORT")
-ESCAPED_RCON_PASSWORD=$(printf '%q' "$RCON_PASSWORD")
 
 # RCON loop with command-line arguments for address and password
 (while read cmd; do
-    rcon -s -a "localhost:$ESCAPED_RCON_PORT" -p "$ESCAPED_RCON_PASSWORD" "$cmd"
+    rcon -s -a "localhost:$RCON_PORT" -p "$RCON_PASSWORD" "$cmd"
 done) < /dev/stdin &
 
 # Replace Startup Variables
